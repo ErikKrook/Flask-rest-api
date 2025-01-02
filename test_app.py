197a3c2630799223088c79b6f63f2105113ce09d
Erik Krook <erikkrook@hotmail.se>
2025-01-02 13:02:13 +0100

    Added a unittest file

---
+++ test_app.py
@@ -0,0 +1,24 @@
+import unittest
+
+from app import app
+
+class TestFlaskApp(unittest.TestCase):
+
+    def setUp(self):
+        # Create a test client for the Flask app
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    def test_get_task_not_found(self):
+        response = self.app.get('/task/10')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json, {"message": "Task not found"})
+
+    def test_post_task(self):
+        response = self.app.post('/task/10', json={"description":"Test Task"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json, {"10":"Test Task"})
+
+
+if __name__ == '__main__':
+    unittest.main()
