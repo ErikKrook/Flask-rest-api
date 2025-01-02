5731c643909705a22168e10ced7a88adfc8aaf1d
Erik Krook <erikkrook@hotmail.se>
2025-01-02 19:32:19 +0100

    Added even more unittests

--- test_app.py
+++ test_app.py
@@ -9,24 +9,50 @@ class TestFlaskApp(unittest.TestCase):
         self.app.testing = True 
 
     def test_get_task_not_found(self):
-        response = self.app.get('/task/10')
+        response = self.app.get('/task/1')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json, {"message": "Task not found"})
 
+    def test_get_task(self):
+        self.app.post('/task/2', json={"description":"Test Task"})
+        response = self.app.get('/task/2')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"2":"Test Task"})
+
     def test_post_task(self):
-        response = self.app.post('/task/10', json={"description":"Test Task"})
+        response = self.app.post('/task/3', json={"description":"Test Task"})
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json, {"10":"Test Task"})
+        self.assertEqual(response.json, {"3":"Test Task"})
+
+    def test_post_task_already_exists(self):
+        self.app.post('/task/4', json={"description":"Test Task"})
+        response = self.app.post('/task/4', json={"description":"Test Task"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"message":"Task already exists"})
+
+    def test_post_task_wrong_key(self):
+        response = self.app.post('/task/5', json={"desciption":"Test Task"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"message":"Missing 'description' key in request body"})  
 
     def test_put_task_not_found(self):
-        response = self.app.put('/task/1', json={"description":"Test Task"})
+        response = self.app.put('/task/6', json={"description":"Test Task"})
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json, {"message":"Task not found"})
+
+    def test_put_task(self):
+        self.app.post('/task/7', json={"description":"Test Task"})
+        response = self.app.put('/task/7', json={"description":"Test Task Update"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"message":"Task updated successfully",
+                                        "task_id": "7",
+                                        "description": "Test Task Update"})
     
     def test_delete_task_not_found(self):
-        response = self.app.delete('/task/1')
+        response = self.app.delete('/task/5')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json, {"message":"Task not found"})
+
         
 if __name__ == '__main__':
     unittest.main()
