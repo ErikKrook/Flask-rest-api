13ec19d5bd297ea4e973e9dd4859e401b07a493d
Erik Krook <erikkrook@hotmail.se>
2025-01-03 11:58:19 +0100

    merge write and append to one file

--- append_to_json.py
+++ append_to_json.py
@@ -4,9 +4,8 @@ import json
 def append_json(new_data, filename='tasks.json'):
     with open(filename, 'r+') as file:
 
-            # Load existing data
+        # Load existing data
         file_data = json.load(file)
-
 
         # Append the new data
         file_data.update(new_data)
@@ -16,6 +15,12 @@ def append_json(new_data, filename='tasks.json'):
         json.dump(file_data, file, indent=4)
         # Truncate file to avoid leftover characters
         file.truncate()
+
+def write_json(new_data, file_name='tasks.json'):
+
+    #Over write existing file
+    with open(file_name, 'w') as outfile:
+        json.dump(new_data, outfile)
 
 # Python object to be appended
 task = {"5": "Walk"}
