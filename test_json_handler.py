34f33c9d267a96b10f7a5d14ff875a133d924aea
Erik Krook <erikkrook@hotmail.se>
2025-01-04 12:11:30 +0100

    added delete function and its test cases

--- test_json_handler.py
+++ test_json_handler.py
@@ -1,12 +1,12 @@
 import unittest
-from append_to_json import update_json
+from json_handler import update_json, read_json, delete_json
 import json
 
 class TestFileOperations(unittest.TestCase):
     def setUp(self):
         self.test_file = 'test_tasks.json'
         with open(self.test_file, 'w') as file:
-            json.dump({"1": "Task1"}, file)
+            json.dump({"1": "Task1", "4": "Task4", "5":"Task5"}, file)
 
     def tearDown(self):
         import os
@@ -18,7 +18,7 @@ class TestFileOperations(unittest.TestCase):
         response = update_json(data, 0, self.test_file)
         self.assertEqual(response, True)
     
-    def test_append_fail(self):
+    def test_append_already_existing_key(self):
         # Test to append a value where a key already exists
         data = {"1":"Test append"}
         response = update_json(data, 0, self.test_file)
@@ -32,11 +32,29 @@ class TestFileOperations(unittest.TestCase):
 
     def test_update_non_existing_key(self):
         # Test to update an nonexisting value
-        data = {"4":"Test Update"}
+        data = {"3":"Test Update"}
         response = update_json(data, 1, self.test_file)
         self.assertEqual(response, False)
-        
 
+    def test_read(self):
+        # Test reading a value
+        response = read_json("4", "test_tasks.json")
+        self.assertEqual(response, "Task4")
+
+    def test_read_non_existing_key(self):
+        # Test reading a non existing value
+        response = read_json("3", "test_tasks.json")
+        self.assertEqual(response, None)
+
+    def test_delete(self):
+        # Test to delete on row
+        response = delete_json("5", "test_tasks.json")
+        self.assertEqual(response, True)
+
+    def test_delete_non_existing_key(self):
+        # Test deleting a non existing value
+        response = delete_json("3", "test_tasks.json")
+        self.assertEqual(response, False)
 
 if "__main__" == __name__:
     unittest.main()
